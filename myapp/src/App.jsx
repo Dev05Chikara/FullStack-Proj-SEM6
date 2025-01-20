4f0d114e27747f2cf43e4b6b4d352dcceddedca2
lokendra <rawat.lokendra2004@gmail.com>
2025-01-20 10:58:52 +0530

    Add files via upload

---
+++ myapp/src/App.jsx
@@ -0,0 +1,19 @@
+import Header from "./components/Header"
+import Buttonnav from "./components/Buttonnav";
+
+function App() {
+  
+
+  return (
+   
+     <div className="App">
+     <Header/>
+     <Buttonnav/>
+     </div>
+    
+     
+   
+  );
+}
+
+export default App
---
+++ myapp/src/components/Buttonnav.jsx
@@ -0,0 +1,27 @@
+import { Box, Button } from '@mui/material'
+import React from 'react'
+import TranslateIcon from '@mui/icons-material/Translate';
+import ImageIcon from '@mui/icons-material/Image';
+import InsertDriveFileIcon from '@mui/icons-material/InsertDriveFile';
+import SpaceDashboardIcon from '@mui/icons-material/SpaceDashboard';
+function Buttonnav() {
+    const buttonStyle={
+        textTransform:"none",
+        fontWeight:"bold",
+        borderColor:"#e3e3e3",
+        marginRight:"8px"
+    }
+  return (
+  < Box sx={{margin:"1rem",
+    paddingLeft:"20px",
+  }}>
+  <Button  sx={buttonStyle} variant="outlined" startIcon={<TranslateIcon/>}>Text</Button>
+  <Button sx={buttonStyle} variant="outlined" startIcon={<ImageIcon/>}>Image</Button>
+  <Button sx={buttonStyle} variant="outlined" startIcon={<InsertDriveFileIcon/>}>Document</Button>
+  <Button  sx={buttonStyle} variant="outlined" startIcon={<SpaceDashboardIcon/>}>Website</Button>
+  
+  </Box>
+  )
+}
+
+export default Buttonnav
